332733bf9e9f8666ce62cf3272ca470805c53ead
toukii <toukii@yahoo.com>
2016-09-02 14:46:05 +0800

    auto

--- routers/commentsRouter_github_com_shaalx_bbs_controllers.go
+++ routers/commentsRouter_github_com_shaalx_bbs_controllers.go
@@ -6,91 +6,91 @@ import (
 
 func init() {
 
-	beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"],
+	beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"],
 		beego.ControllerComments{
 			"Get",
 			`/`,
 			[]string{"get"},
 			nil})
 
-	beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"],
+	beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"],
 		beego.ControllerComments{
 			"LoadLogin",
 			`/login`,
 			[]string{"get"},
 			nil})
 
-	beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"],
+	beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"],
 		beego.ControllerComments{
 			"Login",
 			`/login`,
 			[]string{"post"},
 			nil})
 
-	beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"],
+	beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"],
 		beego.ControllerComments{
 			"LoadPublishTopic",
 			`/publish`,
 			[]string{"get"},
 			nil})
 
-	beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"],
+	beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"],
 		beego.ControllerComments{
 			"PublishTopic",
 			`/publish`,
 			[]string{"post"},
 			nil})
 
-	beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"],
+	beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"],
 		beego.ControllerComments{
 			"RemarkTopic",
 			`/remark/:id:int`,
 			[]string{"post"},
 			nil})
 
-	beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"],
+	beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"],
 		beego.ControllerComments{
 			"ShowTopic",
 			`/topic/:id:int`,
 			[]string{"get"},
 			nil})
 
-	beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"],
+	beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"],
 		beego.ControllerComments{
 			"Logout",
 			`/logout`,
 			[]string{"get"},
 			nil})
 
-	beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"],
+	beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"],
 		beego.ControllerComments{
 			"DelRemark",
 			`/topic/:topicid:int/delremark/:remarkid:int`,
 			[]string{"get"},
 			nil})
 
-	beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"],
+	beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"],
 		beego.ControllerComments{
 			"DelTopic",
 			`/deltopic/:topicid:int`,
 			[]string{"get"},
 			nil})
 
-	beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:MainController"],
+	beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"] = append(beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:MainController"],
 		beego.ControllerComments{
 			"User",
 			`/user`,
 			[]string{"get"},
 			nil})
 
-	beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:RegisterController"] = append(beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:RegisterController"],
+	beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:RegisterController"] = append(beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:RegisterController"],
 		beego.ControllerComments{
 			"LoadRegister",
 			`/register`,
 			[]string{"get"},
 			nil})
 
-	beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:RegisterController"] = append(beego.GlobalControllerRouter["github.com/shaalx/bbs/controllers:RegisterController"],
+	beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:RegisterController"] = append(beego.GlobalControllerRouter["github.com/toukii/bbs/controllers:RegisterController"],
 		beego.ControllerComments{
 			"Register",
 			`/register`,
